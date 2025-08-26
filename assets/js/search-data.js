86e977e232793033d465328404123640e42b4ae1
brockbenton <86993296+brockbenton@users.noreply.github.com>
2025-08-26 20:17:52 +0000

    Deploying to gh-pages from @ brockbenton/brockbenton.github.io@b1e682be9f76b2259dac3187628b8d4fc2b64cf9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/assets/pdf/resume/resume.pdf";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-welcome-to-al-folio",
+        
+          title: "Welcome to Al-Folio!",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/test-post/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {

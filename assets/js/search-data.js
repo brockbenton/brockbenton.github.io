cd2af99b9544ba9c619d6556c9adffe9c4163610
brockbenton <86993296+brockbenton@users.noreply.github.com>
2025-09-18 19:27:56 +0000

    Deploying to gh-pages from @ brockbenton/brockbenton.github.io@7273154b4c8aeaa27a02d7f5178e7200758ace9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/resume/resume.pdf";
+            window.location.href = "/assets/pdf/resume/jtresume.pdf";
           },
         },{id: "post-welcome-to-al-folio",
         

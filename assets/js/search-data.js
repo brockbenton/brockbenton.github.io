ba0cc2db4c25c48dabcacf7115bdd1b77c9a7a98
brockbenton <86993296+brockbenton@users.noreply.github.com>
2025-11-15 23:51:53 +0000

    Deploying to gh-pages from @ brockbenton/brockbenton.github.io@6a350544ff397ecb390bef4e0b64253ff8024d4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/notes/index.html";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-resume",
           title: "resume",
           description: "",
